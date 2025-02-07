a34e3543729a73c2d9633c4c0fc5dec9f70eab41
VolumiDev <diego.martga@gmail.com>
2025-02-07 13:49:55 +0100

    añadimos estilos a las listas

--- Silas/Models/Offers/Offer.cs
+++ Silas/Models/Offers/Offer.cs
@@ -5,7 +5,7 @@
         public int id { get; set; }
         public string title { get; set; }
         public string description { get; set; }
-        public string requiriments { get; set; }
+        public int id_course { get; set; }
         public DateTime date { get; set; }
         public string location { get; set; }
         public int id_company { get; set; }
